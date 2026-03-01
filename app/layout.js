a4a1da7b2ebd95b0a81518fc6486e627afd9828c
dafariel278 <dafarielreal@gmail.com>
2026-03-01 11:12:18 +0700

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "DAFALABS Web3 AI",
+  description: "Web3 Intelligence Agent"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
+}
