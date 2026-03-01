4a2b368eca10679a8db58fd8cd5aaf320773a6e8
dafariel278 <dafarielreal@gmail.com>
2026-03-01 11:11:39 +0700

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div style={{
+      minHeight: "100vh",
+      background: "black",
+      color: "white",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontFamily: "sans-serif"
+    }}>
+      <h1>DAFALABS Web3 AI Agent</h1>
+    </div>
+  );
+}
